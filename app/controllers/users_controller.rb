6424288cb1d396939e6f20b3272b1ec733ba90bc
Ifeanyi Osinakayah Joseph <ifeanyiosinakayah15@gmail.com>
2019-01-30 11:35:00 +0100

    Designed users MVC

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  include UsersHelper
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      @userParams=user_params[:user]
+      render :new
+    end
+  end
 end
