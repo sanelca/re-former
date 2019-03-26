9f125714275872442ef08d85a9f77b08765a4ef2
Sanel Hodzic <sanel_hodzic_tz_91@hotmail.com>
2019-03-26 13:45:40 +0100

    Reviewed tasks finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,7 @@
 class UsersController < ApplicationController
   include UsersHelper
-  def index
-  end
-
   def new
+    @user = User.new
   end
 
   def create
@@ -13,6 +11,19 @@ class UsersController < ApplicationController
     else
       @userParams=user_params[:user]
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:create, :new, :edit, :update]
   root to: 'users#new'
 end
